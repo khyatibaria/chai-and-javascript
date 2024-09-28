1ea502240b7165e53cfe1d51aed365b0851e7b84
Khyati Baria <67405584+khyatibaria@users.noreply.github.com>
2024-09-28 15:46:12 +0000

    feat:01 variables

---
+++ 1_Basics/Variables.js
@@ -0,0 +1,10 @@
+console.log("Khyati");
+
+const accountNumber = 12345678
+let accountName = "Khyati Baria"
+var accountPassword ="12345"
+
+// we can also declare a varibale without even using variable keyword
+accountCity = "Mumbai"
+
+// Prefer not to use var because of functional and block scope.
